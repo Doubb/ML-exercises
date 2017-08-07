23e963e2e69743bad658114bfe6323f69798073e
Oneee <doubele112@naver.com>
2017-08-07 17:06:49 +0900

    Minimize cost function using gradient descent algorithm.
    This code is comparing the two results from manually edited gradient and automatically calculated gradient by using compute_gradient function from optimizer.

--- ML03_Minimize cost function.py
+++ ML03_Minimize cost function.py
@@ -9,14 +9,18 @@ w = tf.Variable(5.0)
 
 h = x*w
 
+gradient = tf.reduce_mean((w*x-y)*x)*2
+
 cost = tf.reduce_mean(tf.square(h-y))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-train = optimizer.minimize(cost)
+
+gvs = optimizer.compute_gradients(cost)
+apply_gradient = optimizer.apply_gradients(gvs)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for step in range(21):
-    print(step,sess.run(w))
-    sess.run(train)
+for step in range(100):
+    print(step,sess.run([gradient,w,gvs]))
+    sess.run(apply_gradient)
